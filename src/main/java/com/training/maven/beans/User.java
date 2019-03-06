735a5325416135a1a3f4a4aaf4d289dbf66af117
finney936 <winny.finney@gmail.com>
2019-03-06 18:30:10 +0530

    Minor cleaning up after previous merge
    Minor cleaning up after previous merge

--- src/main/java/com/training/maven/beans/User.java
+++ src/main/java/com/training/maven/beans/User.java
@@ -1,17 +1,10 @@
 package com.training.maven.beans;
 
-<<<<<<< HEAD
-import org.springframework.beans.factory.InitializingBean;
-
-public class User{
-=======
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name="users")
 public class User {
->>>>>>> servlet-based-config-web-xml
 
 	@Id
 	private String username;
--- src/main/java/com/training/maven/dao/Dao.java
+++ src/main/java/com/training/maven/dao/Dao.java
@@ -1,17 +1,12 @@
 package com.training.maven.dao;
 
-<<<<<<< HEAD
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-=======
 import java.util.ArrayList;
->>>>>>> servlet-based-config-web-xml
 
 import javax.persistence.Query;
 
@@ -19,18 +14,19 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import com.training.maven.beans.Address;
 import com.training.maven.beans.User;
 
 public class Dao {
 
-<<<<<<< HEAD
 	@Autowired
-	static
-	User user;
-	
+	static User user;
+
+	static Configuration configuration = new Configuration().configure().addAnnotatedClass(User.class);
+
+	@Autowired
+	static UserRepo repo;
+
 	public static Connection getConnection() {
 
 		try {
@@ -39,56 +35,42 @@ public class Dao {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 		String user = "system";
 		String password = "Blackshadow936";
 		Connection connection = null;
-		
+
 		try {
 			connection = DriverManager.getConnection(url, user, password);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return connection;
-		
+
 	}
 
-	public static boolean insertUser(User user) {
-		
-		String query = "insert into users (username, password) values ('" + user.getUsername() + "', '" + user.getPassword() + "')";
-		
-		Connection connection = getConnection();
-		
-		int i = 0;
-		try {
-			Statement statement = connection.createStatement();
-			
-			i = statement.executeUpdate(query);
-			// Releasing resources
-			statement.close();
-			connection.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
-		
-		if(i > 0)
-			return true;
-		else
-			return false;
-=======
-	static Configuration configuration = new Configuration().configure().addAnnotatedClass(User.class);
+	/*
+	 * public static boolean insertUser(User user) {
+	 * 
+	 * String query = "insert into users (username, password) values ('" +
+	 * user.getUsername() + "', '" + user.getPassword() + "')";
+	 * 
+	 * Connection connection = getConnection();
+	 * 
+	 * int i = 0; try { Statement statement = connection.createStatement();
+	 * 
+	 * i = statement.executeUpdate(query); // Releasing resources statement.close();
+	 * connection.close(); } catch (SQLException e) { // TODO Auto-generated catch
+	 * block e.printStackTrace(); return false; }
+	 * 
+	 * if(i > 0) return true; else return false; }
+	 */
 
-	@Autowired
-	static
-	UserRepo repo;
-	
-	public static void insertUser(User user) {
-		
+	public static boolean insertUser(User user) {
+
 //		SessionFactory sessionFactory = configuration.buildSessionFactory();
 //		
 //		Session session = sessionFactory.openSession();
@@ -98,77 +80,64 @@ public class Dao {
 //		session.save(user);
 //		
 //		transaction.commit();
-		
-		repo.save(user);
-	
->>>>>>> servlet-based-config-web-xml
-	}
-	
-	public static User getUser(String username) throws Exception{
-		
-		SessionFactory sessionFactory = configuration.buildSessionFactory();
-		
-		Session session = sessionFactory.openSession();
-		
-		Transaction transaction = session.beginTransaction();
-		
-<<<<<<< HEAD
-		ResultSet set = statement.executeQuery(query);
 
-		while(set.next()) {
-			user.setUsername(set.getString("username"));
-			user.setPassword(set.getString("password"));
-		}
-
-		// Releasing resources
-		statement.close();
-		connection.close();
-=======
-		User user = session.get(User.class, username);
-		
-		transaction.commit();
->>>>>>> servlet-based-config-web-xml
-		
-		if(user != null) {
-			return user;
-		}
-		else
-			return new User();
-	}
-	
-/*	public static boolean deleteUser(String username) throws Exception{
-		
-		String query = "DELETE FROM users WHERE username='"+ username +"'";
-		
-		Connection connection = getConnection();
-		
-		Statement statement = connection.createStatement();
-		
-		int i = statement.executeUpdate(query);
-
-		// Releasing resources
-		statement.close();
-		connection.close();
-		
-		if(i > 0)
+		if(repo.save(user) != null)
 			return true;
 		else
 			return false;
-	}*/
-	
-	public static ArrayList<User> userList(){
-		
-		ArrayList<User> list;
-		
+
+	}
+
+	public static User getUser(String username) throws Exception {
+
 		SessionFactory sessionFactory = configuration.buildSessionFactory();
-		
+
 		Session session = sessionFactory.openSession();
-		
+
 		Transaction transaction = session.beginTransaction();
-		
+
+		User user = session.get(User.class, username);
+
+		transaction.commit();
+
+		if (user != null) {
+			return user;
+		} else
+			return new User();
+	}
+
+	/*
+	 * public static boolean deleteUser(String username) throws Exception{
+	 * 
+	 * String query = "DELETE FROM users WHERE username='"+ username +"'";
+	 * 
+	 * Connection connection = getConnection();
+	 * 
+	 * Statement statement = connection.createStatement();
+	 * 
+	 * int i = statement.executeUpdate(query);
+	 * 
+	 * // Releasing resources statement.close(); connection.close();
+	 * 
+	 * if(i > 0) return true; else return false; }
+	 */
+
+	@SuppressWarnings("unchecked")
+	public static ArrayList<User> userList() {
+
+		ArrayList<User> list;
+
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		Session session = sessionFactory.openSession();
+
+		Transaction transaction = session.beginTransaction();
+
 		Query query = session.createQuery("from users");
-		
-		list = (ArrayList<User>)query.getResultList();
+
+		list = (ArrayList<User>) query.getResultList();
+
+		transaction.commit();
 		
 		return list;
 	}
